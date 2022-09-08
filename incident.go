ffdcfbddf2edcf9ffe2d84432da45eaa6f73de88
Konstantin Morozov <morozov.cookie@gmail.com>
2022-09-08 20:54:05 +0300

    domain: add Incident entity

---
+++ incident.go
@@ -0,0 +1,29 @@
+package cm
+
+import (
+	"context"
+	"time"
+)
+
+type Incident struct {
+	// ID is the incident unique identifier.
+	ID ID
+
+	// Summary is the short text describes the change.
+	Summary string
+
+	// Description is the full free form text describes the change.
+	Description string
+
+	// CreatedAt is the UTC time when incident was created.
+	CreatedAt time.Time
+
+	// UpdatedAt is the UTC time when incident was updated.
+	UpdatedAt time.Time
+}
+
+// IncidentService represents a service for managing incidents.
+type IncidentService interface {
+	// CreateIncident creates a new incident.
+	CreateIncident(ctx context.Context, incident *Incident) error
+}
