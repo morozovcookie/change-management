a0118c51dc5e82832d4a594a057afd5464ade48f
Konstantin Morozov <morozov.cookie@gmail.com>
2022-09-09 19:56:21 +0300

    docker: complete compose file

--- scripts/docker/postgres/docker-entrypoint.sh
+++ scripts/docker/postgres/docker-entrypoint.sh
@@ -5,10 +5,12 @@ set -e
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
 	CREATE USER controller WITH PASSWORD 'controller';
 	CREATE DATABASE cm;
-	GRANT ALL PRIVILEGES ON DATABASE cm TO controller;
+	GRANT CONNECT ON DATABASE cm TO controller;
 EOSQL
-
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "cm" <<-EOSQL
 	CREATE SCHEMA controller;
+	GRANT ALL PRIVILEGES ON SCHEMA controller TO controller;
+	GRANT DELETE, INSERT, SELECT, UPDATE ON ALL TABLES IN SCHEMA controller TO controller;
+	GRANT SELECT, USAGE ON ALL SEQUENCES IN SCHEMA controller TO controller;
 EOSQL
