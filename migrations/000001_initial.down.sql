85337c25760ba366c60a1a438e8f3ff8d0d02499
Konstantin Morozov <morozov.cookie@gmail.com>
2023-01-15 18:38:55 +0300

    clean branch

--- migrations/000001_initial.down.sql
+++
@@ -1,7 +0,0 @@
-BEGIN;
-
-    DROP TABLE incidents;
-    DROP TABLE change_requests;
-    DROP TYPE change_request_type;
-
-COMMIT;
--- migrations/000002_add_change_request_queue.down.sql
+++
@@ -1,5 +0,0 @@
-BEGIN;
-    DROP TABLE incident_queue;
-    DROP TABLE change_request_queue;
-    DROP TABLE queue;
-COMMIT;
